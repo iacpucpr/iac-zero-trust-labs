1df981dd9482472067de4c8d4970ba55b22ae774
Juliano S Langaro <137821853+jslangaro@users.noreply.github.com>
2025-07-07 13:35:51 -0300

    Update variables.tf

--- lab-iac-zero-trust/etapa2-terraform/variables.tf
+++ lab-iac-zero-trust/etapa2-terraform/variables.tf
@@ -4,9 +4,18 @@ variable "project_name" {
   type        = string
   default     = "lab-zero-trust"
 }
-
 variable "twingate_connector_token" {
   description = "Registration token for the Twingate Connector (optional)"
   type        = string
   default     = ""
+}
+variable "twingate_login" {
+  description = "Twingate Login NAME.twingate.com"
+  type = string
+  default  = "iacpucpr"
+}
+variable "twingate_api-token" {
+  description = "Twingate API-TOKEN provided by twingate"
+  type = string
+  default  = "TOKEN"
 }
